3efd9dcf03232b260d945892e86f366c99c1cecc
hi-rustin <rustin.liu@gmail.com>
2023-10-02 19:52:01 +0800

    Add the first line

--- src/ops/info.rs
+++ src/ops/info.rs
@@ -1,5 +1,7 @@
+use anstyle::{AnsiColor, Effects};
+use cargo::util::style::{GOOD, NOP, WARN};
 use cargo::{CargoResult, Config};
-use crates_io_api::{CrateResponse, SyncClient};
+use crates_io_api::{FullCrate, SyncClient};
 use std::time::Duration;
 
 pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
@@ -8,11 +10,48 @@ pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
         Duration::from_millis(10),
     )?;
 
-    let krate: CrateResponse = client.get_crate(spec)?;
+    let krate: FullCrate = client.full_crate(spec, false)?;
 
+    pretty_view(&krate, config)?;
+
+    Ok(())
+}
+
+fn pretty_view(krate: &FullCrate, config: &Config) -> CargoResult<()> {
+    config.shell().write_stdout("\n", &NOP)?;
+    config.shell().write_stdout(
+        format!(
+            "{name}@{version}",
+            name = krate.name,
+            version = krate.max_version
+        ),
+        &GOOD,
+    )?;
+    config.shell().write_stdout(" | ", &NOP)?;
+    match krate.license {
+        Some(ref license) => {
+            config
+                .shell()
+                .write_stdout(format!("{license}", license = license), &GOOD)?;
+        }
+        None => {
+            config.shell().write_stdout("No license", &WARN)?;
+        }
+    }
+    config.shell().write_stdout(" | ", &NOP)?;
+    config.shell().write_stdout("deps: ", &NOP)?;
+    let deps = krate.reverse_dependencies.meta.total;
     config
         .shell()
-        .write_stdout(format!("{:?}", krate), &cargo::util::style::NOP)?;
+        .write_stdout(deps, &AnsiColor::Cyan.on_default().effects(Effects::BOLD))?;
+    config.shell().write_stdout(" | ", &NOP)?;
+    config.shell().write_stdout("downloads: ", &NOP)?;
+
+    config.shell().write_stdout(
+        krate.total_downloads,
+        &AnsiColor::Cyan.on_default().effects(Effects::BOLD),
+    )?;
+    config.shell().write_stdout("\n", &NOP)?;
 
     Ok(())
 }
