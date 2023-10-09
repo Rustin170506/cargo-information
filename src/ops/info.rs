01bc14244a1c8c0a5714690f459b52ce7a6d1f2f
hi-rustin <rustin.liu@gmail.com>
2023-10-09 10:11:51 +0800

    feat: Get the latest version
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/ops/info.rs
+++ src/ops/info.rs
@@ -1,5 +1,8 @@
+use std::collections::HashSet;
+
 use cargo::core::registry::PackageRegistry;
-use cargo::core::{PackageId, SourceId};
+use cargo::core::{Dependency, SourceId};
+use cargo::sources::source::QueryKind;
 use cargo::{CargoResult, Config};
 
 pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
@@ -9,7 +12,23 @@ pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
     // Make sure we get the lock before we download anything.
     let _lock = config.acquire_package_cache_lock()?;
 
-    let package_id = PackageId::new(spec, "1.0.0", source_id).unwrap();
+    let mut source = source_id.load(&config, &HashSet::new())?;
+    let dep = Dependency::parse(spec, None, source_id)?;
+
+    let summaries = loop {
+        // Exact to avoid returning all for path/git
+        match source.query_vec(&dep, QueryKind::Exact) {
+            std::task::Poll::Ready(res) => {
+                break res?;
+            }
+            std::task::Poll::Pending => source.block_until_ready()?,
+        }
+    };
+
+    // Get the latest version.
+    let summary = summaries.into_iter().next().unwrap();
+
+    let package_id = summary.package_id();
 
     let package = registry.get(&[package_id])?;
 
