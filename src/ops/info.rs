4683d7a63bb302c00f189b76a2aad1052d7ad512
hi-rustin <rustin.liu@gmail.com>
2023-10-09 09:04:54 +0800

    fix: use correct API and add lock
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/ops/info.rs
+++ src/ops/info.rs
@@ -1,19 +1,21 @@
 use cargo::core::registry::PackageRegistry;
-use cargo::core::PackageId;
+use cargo::core::{PackageId, SourceId};
 use cargo::{CargoResult, Config};
 
 pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
+    let source_id = SourceId::crates_io(config)?;
     let mut registry = PackageRegistry::new(config)?;
-    let source_id = config.crates_io_source_id()?;
+    registry.add_sources([source_id])?;
+    // Make sure we get the lock before we download anything.
+    let _lock = config.acquire_package_cache_lock()?;
+
     let package_id = PackageId::new(spec, "1.0.0", source_id).unwrap();
 
-    registry.add_sources([source_id])?;
+    let package = registry.get(&[package_id])?;
 
-    let p = registry.get(&[package_id])?;
+    let package = package.get_one(package_id)?;
 
-    let p = p.get_one(package_id)?;
-
-    println!("{:?}", p.manifest());
+    println!("{:?}", package.manifest());
 
     Ok(())
 }
