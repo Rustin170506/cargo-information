80105b1d2aa56dc6d82f06d15d978a306a12205f
hi-rustin <rustin.liu@gmail.com>
2023-10-10 09:50:44 +0800

    feat: print first line
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/ops/info.rs
+++ src/ops/info.rs
@@ -1,8 +1,10 @@
 use std::collections::HashSet;
 
+use anstyle::{AnsiColor, Effects};
 use cargo::core::registry::PackageRegistry;
-use cargo::core::{Dependency, SourceId};
+use cargo::core::{Dependency, Package, SourceId};
 use cargo::sources::source::QueryKind;
+use cargo::util::style::{GOOD, NOP, WARN};
 use cargo::{CargoResult, Config};
 
 pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
@@ -25,8 +27,11 @@ pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
         }
     };
 
-    // Get the latest version.
-    let summary = summaries.into_iter().next().unwrap();
+    // Find the latest version.
+    let summary = summaries
+        .iter()
+        .max_by_key(|s| s.package_id().version())
+        .unwrap();
 
     let package_id = summary.package_id();
 
@@ -34,7 +39,44 @@ pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
 
     let package = package.get_one(package_id)?;
 
-    println!("{:?}", package.manifest());
+    pretty_view(&package, config)?;
+
+    Ok(())
+}
+
+fn pretty_view(krate: &Package, config: &Config) -> CargoResult<()> {
+    let summary = krate.manifest().summary();
+    let package_id = summary.package_id();
+    let manmeta = krate.manifest().metadata();
+
+    config.shell().write_stdout("\n", &NOP)?;
+    config.shell().write_stdout(
+        format!(
+            "{name}@{version}",
+            name = package_id.name(),
+            version = package_id.version()
+        ),
+        &GOOD,
+    )?;
+    config.shell().write_stdout(" | ", &NOP)?;
+    match manmeta.license {
+        Some(ref license) => {
+            config
+                .shell()
+                .write_stdout(format!("{license}", license = license), &GOOD)?;
+        }
+        None => {
+            config.shell().write_stdout("No license", &WARN)?;
+        }
+    }
+    config.shell().write_stdout(" | ", &NOP)?;
+    config.shell().write_stdout("deps: ", &NOP)?;
+    let deps = summary.dependencies().len();
+    config
+        .shell()
+        .write_stdout(deps, &AnsiColor::Cyan.on_default().effects(Effects::BOLD))?;
+
+    config.shell().write_stdout("\n", &NOP)?;
 
     Ok(())
 }
