7bda3ad8d52b3ee98f2f86437253d70969c74a9d
hi-rustin <rustin.liu@gmail.com>
2023-11-03 10:46:44 +0800

    feat: support search in workspace
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/ops/info.rs
+++ src/ops/info.rs
@@ -1,11 +1,54 @@
 use cargo::core::registry::PackageRegistry;
-use cargo::core::{Dependency, QueryKind, Registry, SourceId};
-use cargo::{CargoResult, Config};
+use cargo::core::{Dependency, QueryKind, Registry, SourceId, Workspace};
+use cargo::util::command_prelude::root_manifest;
+use cargo::{ops, CargoResult, Config};
 
 use super::view::pretty_view;
 
 /// Check the information about a package.
 pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
+    match root_manifest(None, config) {
+        Ok(root) => {
+            let ws = Workspace::new(&root, config);
+            match ws {
+                Ok(ws) => search_in_workspace(spec, &ws, config),
+                Err(_) => {
+                    // If we can't load the workspace, then we're not in a workspace
+                    // and we should search crates.io.
+                    search_out_of_workspace(spec, config)
+                }
+            }
+        }
+        Err(_) => return search_out_of_workspace(spec, config),
+    }
+}
+
+fn search_in_workspace(spec: &str, ws: &Workspace<'_>, config: &Config) -> CargoResult<()> {
+    let mut registry = PackageRegistry::new(config)?;
+    // Make sure we get the lock before we download anything.
+    let _lock = config.acquire_package_cache_lock()?;
+    registry.lock_patches();
+    let resolve = match ops::load_pkg_lockfile(ws)? {
+        Some(resolve) => resolve,
+        None => return Ok(()),
+    };
+
+    let package_id = resolve.query(spec)?;
+
+    let package = registry.get(&[package_id])?;
+
+    let package = package.get_one(package_id)?;
+
+    let mut shell = config.shell();
+    let stdout = shell.out();
+
+    // TODO: fix this summary.
+    pretty_view(package, &[], stdout)?;
+
+    Ok(())
+}
+
+fn search_out_of_workspace(spec: &str, config: &Config) -> CargoResult<()> {
     let source_id = SourceId::crates_io_maybe_sparse_http(config)?;
     let mut registry = PackageRegistry::new(config)?;
 
