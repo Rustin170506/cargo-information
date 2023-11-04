5b38621e596308b4c2e3e57c830b893742e0d6a2
Rustin Liu <rustin.liu@gmail.com>
2023-11-04 17:07:16 +0800

    feat: support search in workspace (#1)

--- src/ops/info.rs
+++ src/ops/info.rs
@@ -1,18 +1,44 @@
 use cargo::core::registry::PackageRegistry;
-use cargo::core::{Dependency, QueryKind, Registry, SourceId};
-use cargo::{CargoResult, Config};
+use cargo::core::{Dependency, PackageId, QueryKind, Registry, SourceId, Workspace};
+use cargo::util::command_prelude::root_manifest;
+use cargo::{ops, CargoResult, Config};
 
 use super::view::pretty_view;
 
 /// Check the information about a package.
 pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
-    let source_id = SourceId::crates_io_maybe_sparse_http(config)?;
     let mut registry = PackageRegistry::new(config)?;
-
     // Make sure we get the lock before we download anything.
     let _lock = config.acquire_package_cache_lock()?;
     registry.lock_patches();
 
+    let mut package_id = None;
+    // If we can find it in workspace, use it as a specific version.
+    if let Ok(root) = root_manifest(None, config) {
+        let ws = Workspace::new(&root, config)?;
+        if let Some(resolve) = ops::load_pkg_lockfile(&ws)? {
+            if let Ok(p) = resolve.query(spec) {
+                package_id = Some(p)
+            }
+        }
+    }
+
+    query_and_pretty_view(spec, package_id, config, registry)
+}
+
+// Query the package registry and pretty print the result.
+// If package_id is None, find the latest version.
+fn query_and_pretty_view(
+    spec: &str,
+    package_id: Option<PackageId>,
+    config: &Config,
+    mut registry: PackageRegistry,
+) -> CargoResult<()> {
+    let source_id = match package_id {
+        Some(package_id) => package_id.source_id(),
+        None => SourceId::crates_io(config)?,
+    };
+    // Query without version requirement to get all index summaries.
     let dep = Dependency::parse(spec, None, source_id)?;
     let summaries = loop {
         // Exact to avoid returning all for path/git
@@ -24,16 +50,17 @@ pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
         }
     };
 
-    // Find the latest version.
-    let summary = summaries
-        .iter()
-        .max_by_key(|s| s.package_id().version())
-        .unwrap();
+    let package_id = package_id.unwrap_or_else(|| {
+        // Find the latest version.
+        let summary = summaries
+            .iter()
+            .max_by_key(|s| s.package_id().version())
+            .unwrap();
 
-    let package_id = summary.package_id();
+        summary.package_id()
+    });
 
     let package = registry.get(&[package_id])?;
-
     let package = package.get_one(package_id)?;
 
     let mut shell = config.shell();
