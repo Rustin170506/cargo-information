dba850b8442ec33281ce89358f51777362ca1e81
hi-rustin <rustin.liu@gmail.com>
2023-11-04 17:01:39 +0800

    Better code
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/ops/info.rs
+++ src/ops/info.rs
@@ -1,5 +1,5 @@
 use cargo::core::registry::PackageRegistry;
-use cargo::core::{Dependency, QueryKind, Registry, SourceId, Workspace};
+use cargo::core::{Dependency, PackageId, QueryKind, Registry, SourceId, Workspace};
 use cargo::util::command_prelude::root_manifest;
 use cargo::{ops, CargoResult, Config};
 
@@ -7,58 +7,36 @@ use super::view::pretty_view;
 
 /// Check the information about a package.
 pub fn info(spec: &str, config: &Config) -> CargoResult<()> {
-    match root_manifest(None, config) {
-        Ok(root) => {
-            let ws = Workspace::new(&root, config)?;
-            search_in_workspace(spec, &ws, config)
-        }
-        Err(_) => search_out_of_workspace(spec, config),
-    }
-}
-
-fn search_in_workspace(spec: &str, ws: &Workspace<'_>, config: &Config) -> CargoResult<()> {
     let mut registry = PackageRegistry::new(config)?;
     // Make sure we get the lock before we download anything.
     let _lock = config.acquire_package_cache_lock()?;
     registry.lock_patches();
-    let resolve = match ops::load_pkg_lockfile(ws)? {
-        Some(resolve) => resolve,
-        None => return Ok(()),
-    };
 
-    let package_id = resolve.query(spec)?;
-
-    let dep = Dependency::parse(package_id.name(), None, package_id.source_id())?;
-    let summaries = loop {
-        // Exact to avoid returning all for path/git
-        match registry.query_vec(&dep, QueryKind::Exact) {
-            std::task::Poll::Ready(res) => {
-                break res?;
+    let mut package_id = None;
+    // If we can find it in workspace, use it as a specific version.
+    if let Ok(root) = root_manifest(None, config) {
+        let ws = Workspace::new(&root, config)?;
+        if let Some(resolve) = ops::load_pkg_lockfile(&ws)? {
+            if let Ok(p) = resolve.query(spec) {
+                package_id = Some(p)
             }
-            std::task::Poll::Pending => registry.block_until_ready()?,
         }
-    };
+    }
 
-    let package = registry.get(&[package_id])?;
-
-    let package = package.get_one(package_id)?;
-
-    let mut shell = config.shell();
-    let stdout = shell.out();
-
-    pretty_view(package, &summaries, stdout)?;
-
-    Ok(())
+    search(spec, package_id, config, registry)
 }
 
-fn search_out_of_workspace(spec: &str, config: &Config) -> CargoResult<()> {
-    let source_id = SourceId::crates_io_maybe_sparse_http(config)?;
-    let mut registry = PackageRegistry::new(config)?;
-
-    // Make sure we get the lock before we download anything.
-    let _lock = config.acquire_package_cache_lock()?;
-    registry.lock_patches();
-
+fn search(
+    spec: &str,
+    package_id: Option<PackageId>,
+    config: &Config,
+    mut registry: PackageRegistry,
+) -> CargoResult<()> {
+    let source_id = match package_id {
+        Some(package_id) => package_id.source_id(),
+        None => SourceId::crates_io(config)?,
+    };
+    // Query without version requirement to get all index summaries.
     let dep = Dependency::parse(spec, None, source_id)?;
     let summaries = loop {
         // Exact to avoid returning all for path/git
@@ -70,16 +48,17 @@ fn search_out_of_workspace(spec: &str, config: &Config) -> CargoResult<()> {
         }
     };
 
-    // Find the latest version.
-    let summary = summaries
-        .iter()
-        .max_by_key(|s| s.package_id().version())
-        .unwrap();
+    let package_id = package_id.unwrap_or_else(|| {
+        // Find the latest version.
+        let summary = summaries
+            .iter()
+            .max_by_key(|s| s.package_id().version())
+            .unwrap();
 
-    let package_id = summary.package_id();
+        summary.package_id()
+    });
 
     let package = registry.get(&[package_id])?;
-
     let package = package.get_one(package_id)?;
 
     let mut shell = config.shell();
