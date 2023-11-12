69d4c9a893239bf5d061823c9f6ea946d2dec3a3
hi-rustin <rustin.liu@gmail.com>
2023-11-12 15:47:40 +0800

    test: add with locked arg cases
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- tests/testsuite/cargo_information/mod.rs
+++ tests/testsuite/cargo_information/mod.rs
@@ -3,5 +3,7 @@ mod help;
 mod not_found;
 mod with_frozen_outside_workspace;
 mod with_frozen_within_workspace;
+mod with_locked_outside_workspace;
+mod with_locked_within_workspace;
 mod with_offline;
 mod within_workspace;
