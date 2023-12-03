c1585558487c1d15f6da3e816fdc9d53c89bb609
hi-rustin <rustin.liu@gmail.com>
2023-12-03 22:12:08 +0800

    test: add registry is not matched case
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- tests/testsuite/cargo_information/mod.rs
+++ tests/testsuite/cargo_information/mod.rs
@@ -6,6 +6,7 @@ mod help;
 mod not_found;
 mod specify_version_outside_workspace;
 mod specify_version_with_url;
+mod specify_version_with_url_but_registry_is_not_matched;
 mod specify_version_with_ws_and_match_multiple_versions;
 mod specify_version_within_ws_and_conflict_with_lockfile;
 mod specify_version_within_ws_and_match_with_lockfile;
