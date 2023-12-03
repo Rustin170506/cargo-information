b0a6836b812f09b180cba90fb89a19549ddae279
hi-rustin <rustin.liu@gmail.com>
2023-12-03 22:07:14 +0800

    test: add URL package spec case
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

---
+++ tests/testsuite/cargo_information/specify_version_with_url/mod.rs
@@ -0,0 +1,18 @@
+use cargo_test_macro::cargo_test;
+use cargo_test_support::curr_dir;
+
+use super::{cargo_info, init_registry_without_token};
+
+#[cargo_test]
+fn case() {
+    init_registry_without_token();
+    cargo_test_support::registry::Package::new("my-package", "0.1.1+my-package").publish();
+
+    cargo_info()
+        .arg("https://crates.io/my-package")
+        .arg("--registry=dummy-registry")
+        .assert()
+        .success()
+        .stdout_matches_path(curr_dir!().join("stdout.log"))
+        .stderr_matches_path(curr_dir!().join("stderr.log"));
+}
